90572bbe5da14c0e15533d2f35b85702917a8bee
RyanBennett98 <ryanbennett@berkeley.edu>
2019-04-30 15:33:06 -0700

    Made sure IDs are unique. Pref names now match actual names

--- gen_students.rb
+++ gen_students.rb
@@ -12,16 +12,17 @@ File.open(path_to_write, "w") do |f|
     # Generate student data
     first_name = Faker::Name.first_name
     last_name = Faker::Name.last_name
+    middle_name = Faker::Name.middle_name
     temp_student_hash = {     
       "UC_SRCH_CRIT" => first_name + " " + last_name,
-      "STUDENT_ID" => Faker::Number.number(11),
-      "CAMPUS_ID" => Faker::Number.number(30), 
-      "OPRID" => Faker::Number.number(30),
+      "STUDENT_ID" => Faker::Number.unique.number(11),
+      "CAMPUS_ID" => Faker::Number.unique.number(30), 
+      "OPRID" => Faker::Number.unique.number(30),
       "LAST_NAME" =>  last_name,
       "FIRST_NAME" =>  first_name,
-      "MIDDLE_NAME" =>  Faker::Name.middle_name,
-      "UC_PRF_FIRST_NM" =>  Faker::Name.first_name,
-      "UC_PRF_MIDDLE_NM" =>  Faker::Name.middle_name,
+      "MIDDLE_NAME" =>  middle_name,
+      "UC_PRF_FIRST_NM" =>  first_name,
+      "UC_PRF_MIDDLE_NM" =>  middle_name,
       "EMAIL_ADDR" => first_name.downcase + last_name.downcase.gsub("'", "") + "@berkeley.edu",
       "ACAD_PROG" => Faker::Job.education_level
     }
