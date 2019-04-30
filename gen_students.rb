c6beef18f1a00f3fecfce14f25a0d60150e9f870
Yunwei Yang <yunweiyang@Yunweis-MacBook-Pro.local>
2019-04-30 10:43:05 -0700

    changed gen_studnets.rb so it matches the table entries

--- gen_students.rb
+++ gen_students.rb
@@ -12,13 +12,18 @@ File.open(path_to_write, "w") do |f|
     # Generate student data
     first_name = Faker::Name.first_name
     last_name = Faker::Name.last_name
-    temp_student_hash = {
-      "student_id" => Faker::Number.number(11),
-      "campus_id" => Faker::Number.number(16),
-      "name" =>  first_name + " " + last_name,
-      "email" => first_name.downcase + last_name.downcase.gsub("'", "") + "@berkeley.edu",
-      "phone" => Faker::PhoneNumber.phone_number
-      # Add other attributes too
+    temp_student_hash = {     
+      "UC_SRCH_CRIT" => first_name + " " + last_name,
+      "STUDENT_ID" => Faker::Number.number(11),
+      "CAMPUS_ID" => Faker::Number.number(30), 
+      "OPRID" => Faker::Number.number(30),
+      "LAST_NAME" =>  last_name,
+      "FIRST_NAME" =>  first_name,
+      "MIDDLE_NAME" =>  Faker::Name.middle_name,
+      "UC_PRF_FIRST_NM" =>  Faker::Name.first_name,
+      "UC_PRF_MIDDLE_NM" =>  Faker::Name.middle_name,
+      "EMAIL_ADDR" => first_name.downcase + last_name.downcase.gsub("'", "") + "@berkeley.edu",
+      "ACAD_PROG" => Faker::Job.education_level
     }
     f.write(JSON.pretty_generate(temp_student_hash))
     f.write("\n")
